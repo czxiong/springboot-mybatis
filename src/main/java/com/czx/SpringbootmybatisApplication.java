6ae98878408106b5227306a082c5a4c8700ffde7
czxiong <247859013@qq.com>
2018-04-13 14:43:34 +0800

    添加注解事务

--- src/main/java/com/czx/SpringbootmybatisApplication.java
+++ src/main/java/com/czx/SpringbootmybatisApplication.java
@@ -3,9 +3,11 @@ package com.czx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.czx.mapper")
+@EnableTransactionManagement //开启注解事务管理
 public class SpringbootmybatisApplication {
 
 	public static void main(String[] args) {
