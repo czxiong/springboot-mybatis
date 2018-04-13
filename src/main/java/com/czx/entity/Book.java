0dbd541bdf8d7a205f659003965bb6bc2fdbfc81
czxiong <247859013@qq.com>
2018-04-13 15:27:14 +0800

    优化

--- src/main/java/com/czx/entity/Book.java
+++ src/main/java/com/czx/entity/Book.java
@@ -1,10 +1,12 @@
 package com.czx.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
 import java.util.Date;
 
+@Data
 public class Book {
 
     private Integer id;
@@ -18,45 +20,4 @@ public class Book {
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss",locale = "zh",timezone = "GMT+8")
     private Date publishTime;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public Date getPublishTime() {
-        return publishTime;
-    }
-
-    public void setPublishTime(Date publishTime) {
-        this.publishTime = publishTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price='" + price + '\'' +
-                ", publishTime=" + publishTime +
-                '}';
-    }
 }
--- src/main/java/com/czx/entity/Result.java
+++ src/main/java/com/czx/entity/Result.java
@@ -1,9 +1,12 @@
 package com.czx.entity;
 
+import lombok.Data;
+
 /**
  * 封装返回的统一对象
  * @param <T>
  */
+@Data
 public class Result<T> {
 
     private Integer code;//错误码
@@ -12,36 +15,4 @@ public class Result<T> {
 
     private T data;//返回的具体内容
 
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        return "Result{" +
-                "code=" + code +
-                ", msg='" + msg + '\'' +
-                ", data=" + data +
-                '}';
-    }
 }
